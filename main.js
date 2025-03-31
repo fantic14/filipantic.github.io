8972792c084354b870253799a4f4c6d065bc4d92
fantic14 <filip.antic14@gmail.com>
2025-03-31 18:44:52 +0200

    added buttons for back to top and next slide

--- main.js
+++ main.js
@@ -1,11 +1,42 @@
+let currentSlide = 0;
+
 document.addEventListener("scroll", () => {
     let scrollProgress = window.scrollY / window.innerHeight;
-    console.log(scrollProgress)
 
     document.querySelector(".start").style.opacity = `${1 - scrollProgress}`;
     if (scrollProgress <= 1)
-        document.querySelector(".description").style.opacity = `${scrollProgress}`;
+        document.querySelector(".description").style.opacity = `${scrollProgress}`+1;
+    else if (scrollProgress >= 1 && scrollProgress <= 1.7)
+        document.querySelector(".description").style.opacity = "1";
     else
-        document.querySelector(".description").style.opacity = `${-scrollProgress+3}`;
-    document.querySelector(".third").style.opacity = `${scrollProgress-2}`;
+        document.querySelector(".description").style.opacity = `${-scrollProgress+2}`;
+    document.querySelector(".third").style.opacity = `${scrollProgress-1}`;
+
+    if (scrollProgress === 0)
+        document.querySelector(".to-top-button").style.opacity = `0`;
+    else
+        document.querySelector(".to-top-button").style.opacity = `1`;
+
+    currentSlide = Math.floor(scrollProgress);
+
+    if (scrollProgress >= 1.9)
+        document.querySelector(".next-page-button").style.opacity = `0`;
+    else
+        document.querySelector(".next-page-button").style.opacity = `1`;
 });
+
+function nextSlide() {
+    if (currentSlide < 2) {
+        currentSlide++;
+        window.scrollTo({top: currentSlide * window.innerHeight, behavior: "smooth"});
+    }
+}
+
+function toTheTop() {
+    window.scrollTo({top: 0, behavior: "smooth"});
+}
+
+// document.addEventListener("keydown", (event) => {
+//     if (event.key === "ArrowDown") nextSlide();
+//     if (event.key === "ArrowUp") prevSlide();
+// });
