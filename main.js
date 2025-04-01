23df0c543eff8ce77313237b843f2324dbd70bc1
fantic14 <filip.antic14@gmail.com>
2025-04-01 17:44:02 +0200

    added navigation buttons in navbar, blocked scrolling with cursor and enabled going through page just by arrows on page, arrows on keyboard and navbar. Added icon on tab

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+document.addEventListener("wheel", (event) => event.preventDefault(), { passive: false }); // Disable mouse wheel
+
 let currentSlide = 0;
 
 document.addEventListener("scroll", () => {
@@ -10,7 +12,7 @@ document.addEventListener("scroll", () => {
         document.querySelector(".description").style.opacity = "1";
     else
         document.querySelector(".description").style.opacity = `${-scrollProgress+2}`;
-    document.querySelector(".third").style.opacity = `${scrollProgress-1}`;
+    document.querySelector(".third").style.opacity = `${scrollProgress-0.5}`;
 
     if (scrollProgress === 0)
         document.querySelector(".to-top-button").style.opacity = `0`;
@@ -25,6 +27,17 @@ document.addEventListener("scroll", () => {
         document.querySelector(".next-page-button").style.opacity = `1`;
 });
 
+document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowDown") {
+        event.preventDefault(); // Prevent default only for navigation keys
+        nextSlide();
+    }
+    if (event.key === "ArrowUp") {
+        event.preventDefault();
+        prevSlide();
+    }
+});
+
 function nextSlide() {
     if (currentSlide < 2) {
         currentSlide++;
@@ -32,11 +45,18 @@ function nextSlide() {
     }
 }
 
+function prevSlide() {
+    if (currentSlide > 0) {
+        currentSlide--;
+        window.scrollTo({top: currentSlide * window.innerHeight, behavior: "smooth"});
+    }
+}
+
+function toSpecificSlide(slideNumber){
+    currentSlide = slideNumber;
+    window.scrollTo({top: currentSlide * window.innerHeight, behavior: "smooth"});
+}
+
 function toTheTop() {
     window.scrollTo({top: 0, behavior: "smooth"});
 }
-
-// document.addEventListener("keydown", (event) => {
-//     if (event.key === "ArrowDown") nextSlide();
-//     if (event.key === "ArrowUp") prevSlide();
-// });
